5270459b944b180ba2b52c49cf203428c0ef2594
Thainan Vinícius Katchan <167592380+ThainanViniciusKatchan@users.noreply.github.com>
2025-03-07 16:00:41 -0400

    Update Config.py

--- app/Config.py
+++ app/Config.py
@@ -4,8 +4,8 @@ from flask_migrate import Migrate
 from itsdangerous import URLSafeTimedSerializer
 
 # declaração das configurações necessárias para criação do banco
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Dados.sqlite3'
-app.config['SECRET_KEY'] = 'db8:y9e~XH.N3FKvy9e~db8:y9e~XH.N3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NomeDoBanco.sqlite3'
+app.config['SECRET_KEY'] = ''
 db = SQLAlchemy(app)
 app.app_context().push()
 migrate = Migrate(app, db, render_as_batch=True)
